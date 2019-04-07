099705f21e2b209f501ac10464c803473f2bad4c
Juliano Pacheco <julianopache@gmail.com>
2019-04-07 15:09:43 -0300

    atualização de arquivos

--- arquivos/json/convert_file.py
+++
@@ -1,7 +0,0 @@
-myFile=open('/Users/julianopacheco/dev/python_workspace/classificacao-textos-juridicos/arquivos/json/crime.json', 'r')
-myObject=myFile.read()
-u = myObject.encode().decode('utf-8-sig')
-myObject = u.encode('utf-8')
-myFile.encoding
-myFile.close()
-myData=json.loads(myObject,'utf-8')
